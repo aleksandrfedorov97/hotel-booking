5495c18244bf926f9e3c30b84df7535b8701ac14
Aleksandr Fedorov <aleksandr.fedorov@onlyoffice.com>
2024-05-27 09:43:54 +0300

    feat: ExceptionHandlerController

---
+++ src/main/java/com/example/hotelbooking/web/controller/ExceptionHandlerController.java
@@ -0,0 +1,41 @@
+package com.example.hotelbooking.web.controller;
+
+import com.example.hotelbooking.exception.UserAlreadyExistsException;
+import com.example.hotelbooking.exception.UserNotFoundException;
+import com.example.hotelbooking.web.dto.error.ErrorResponse;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+
+@RestControllerAdvice
+public class ExceptionHandlerController {
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<ErrorResponse> entityNotFound(UserNotFoundException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(new ErrorResponse(e.getLocalizedMessage()));
+    }
+
+    @ExceptionHandler(UserAlreadyExistsException.class)
+    public ResponseEntity<ErrorResponse> entityNotFound(UserAlreadyExistsException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(new ErrorResponse(e.getLocalizedMessage()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> notValid(MethodArgumentNotValidException e) {
+        BindingResult bindingResult = e.getBindingResult();
+        String message = bindingResult.getAllErrors()
+               .get(0)
+               .getDefaultMessage();
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(new ErrorResponse(message));
+    }
+}
