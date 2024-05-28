0674f0cfd6caf7b85f9390c91d5aeb730d08dbbb
Aleksandr Fedorov <aleksandr.fedorov@onlyoffice.com>
2024-05-28 08:46:14 +0300

    feat: dto and mapper for Room

---
+++ src/main/java/com/example/hotelbooking/web/dto/room/RoomUpsertRequest.java
@@ -0,0 +1,16 @@
+package com.example.hotelbooking.web.dto.room;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@Data
+@NoArgsConstructor
+public class RoomUpsertRequest {
+    private String name;
+    private String description;
+    private String number;
+    private Long price;
+    private Integer capacity;
+}
