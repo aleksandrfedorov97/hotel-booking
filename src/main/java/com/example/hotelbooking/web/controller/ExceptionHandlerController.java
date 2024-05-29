261149a3c9782d1b8a9a86dbfda312b6864fcc92
Aleksandr Fedorov <aleksandr.fedorov@onlyoffice.com>
2024-05-29 08:28:17 +0300

    feat: ExceptionHandlerController (Hotel)

--- src/main/java/com/example/hotelbooking/web/controller/ExceptionHandlerController.java
+++ src/main/java/com/example/hotelbooking/web/controller/ExceptionHandlerController.java
@@ -1,10 +1,10 @@
 package com.example.hotelbooking.web.controller;
 
+import com.example.hotelbooking.exception.HotelNotFoundException;
 import com.example.hotelbooking.exception.RoomNotFoundException;
 import com.example.hotelbooking.exception.UserAlreadyExistsException;
 import com.example.hotelbooking.exception.UserNotFoundException;
 import com.example.hotelbooking.web.dto.error.ErrorResponse;
-import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
@@ -30,6 +30,12 @@ public class ExceptionHandlerController {
 
     @ExceptionHandler(RoomNotFoundException.class)
     public ResponseEntity<ErrorResponse> entityNotFound(RoomNotFoundException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(new ErrorResponse(e.getLocalizedMessage()));
+    }
+
+    @ExceptionHandler(HotelNotFoundException.class)
+    public ResponseEntity<ErrorResponse> entityNotFound(HotelNotFoundException e) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
                 .body(new ErrorResponse(e.getLocalizedMessage()));
     }
