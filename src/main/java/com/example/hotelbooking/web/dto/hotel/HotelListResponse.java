d118d80af202a1ba44ae6ada353fbdc52fa82844
Aleksandr Fedorov <aleksandr.fedorov@onlyoffice.com>
2024-05-29 08:27:55 +0300

    feat: dto and mapper for Hotel

---
+++ src/main/java/com/example/hotelbooking/web/dto/hotel/HotelListResponse.java
@@ -0,0 +1,15 @@
+package com.example.hotelbooking.web.dto.hotel;
+
+import com.example.hotelbooking.entity.Hotel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@AllArgsConstructor
+@Data
+@NoArgsConstructor
+public class HotelListResponse {
+    private List<HotelResponse> hotels;
+}
---
+++ src/main/java/com/example/hotelbooking/web/dto/hotel/HotelUpsertRequest.java
@@ -0,0 +1,11 @@
+package com.example.hotelbooking.web.dto.hotel;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@Data
+@NoArgsConstructor
+public class HotelUpsertRequest {
+}
