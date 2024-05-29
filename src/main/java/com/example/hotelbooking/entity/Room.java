c7bed3d1ccff8b9c5c88981303b780b8adf5c36b
Aleksandr Fedorov <aleksandr.fedorov@onlyoffice.com>
2024-05-29 08:26:47 +0300

    feat: entity Hotel

--- src/main/java/com/example/hotelbooking/entity/Room.java
+++ src/main/java/com/example/hotelbooking/entity/Room.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -24,4 +26,7 @@ public class Room {
     private String number;
     private Long price;
     private Integer capacity;
+    @ManyToOne
+    @JoinColumn(name = "hotel_id")
+    private Hotel hotel;
 }
