47fb3f34414df0c07c87de80e32445c8647a3438
Aleksandr Fedorov <aleksandr.fedorov@onlyoffice.com>
2024-06-04 09:43:53 +0300

    feat: UserDetailsImpl and UserDetailsServiceImpl

---
+++ src/main/java/com/example/hotelbooking/security/UserDetailsServiceImpl.java
@@ -0,0 +1,19 @@
+package com.example.hotelbooking.security;
+
+import com.example.hotelbooking.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UserService userService;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return new UserDetailsImpl(userService.findByUsername(username));
+    }
+}
